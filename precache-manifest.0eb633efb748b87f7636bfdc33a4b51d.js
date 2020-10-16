341a636dad11984de6d205c12198cb3886e3ddcb
Dominika <dominikaperkowska@gmail.com>
2020-10-16 11:57:56 -0400

    Updates

---
+++ precache-manifest.0eb633efb748b87f7636bfdc33a4b51d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95ee9cbf00c81bb76d3b6b2d7dc9026",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "2ca4f0f753e7b1d2833d",
+    "url": "/reactPortfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "473b03a00c840064dbbb",
+    "url": "/reactPortfolio/static/css/main.8ae9df80.chunk.css"
+  },
+  {
+    "revision": "2ca4f0f753e7b1d2833d",
+    "url": "/reactPortfolio/static/js/2.6d7e2ab4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactPortfolio/static/js/2.6d7e2ab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "473b03a00c840064dbbb",
+    "url": "/reactPortfolio/static/js/main.ae65ed3a.chunk.js"
+  },
+  {
+    "revision": "30d8a5e26af80c25b7fa",
+    "url": "/reactPortfolio/static/js/runtime-main.e9519e7c.js"
+  }
+]);
